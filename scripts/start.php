ade635fe61a7ecf1668bd961f816d7299e03a948
Razvan <tangent.jotey@gmail.com>
2022-05-13 00:01:57 +0200

    Update start.php

--- scripts/start.php
+++ scripts/start.php
@@ -7,12 +7,12 @@ chdir(getcwd());
 require 'vendor/autoload.php';
 global $argv;
 
-$filename = $argv[1]??'';
-$dev = 'dev' === ($argv[2]??'');
-$devSleep = !$dev ? 100 : ($argv[3]??100);
+$filename = str_replace("\\", '/', $argv[1] ?? '');
+$dev = 'dev' === ($argv[2] ?? '');
+$devSleep = !$dev ? 100 : ($argv[3] ?? 100);
 
 Bootstrap::start(
 	filename: $filename,
-	dev     : $dev,
+	dev: $dev,
 	devSleep: $devSleep
 );
