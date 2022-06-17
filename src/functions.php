6ea634ec7d138ede96099cb7991c1be85fea2d74
Razvan Tanase <tangent.jotey@gmail.com>
2022-06-18 00:10:00 +0200

    update

---
+++ src/functions.php
@@ -0,0 +1,26 @@
+<?php
+namespace CatPaw;
+
+use function Amp\call;
+use function Amp\File\isDirectory;
+use function Amp\File\listFiles;
+
+use Amp\Promise;
+
+function listFilesRecursive(string $path):Promise {
+    return call(function() use ($path) {
+        $items = yield listFiles($path);
+        $files = [];
+        foreach($items => $item){
+            if(yield isDirectory($item)){
+                foreach(yield listFilesRecursive($item) as $subItem){
+                    $files[] = $subItem;
+                }
+                continue;
+            }
+
+            $files[] = $item;
+        }
+        return $files;
+    });
+}
