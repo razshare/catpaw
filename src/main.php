e06b24f1331a7483e14af651de7273eb37243546
tncrazvan <tangent.jotey@gmail.com>
2022-08-08 01:32:20 +0200

    Adding CatPawDriver and CatPawFile. CatPawDriver can be enabling by setting the env variable "CATPAW_FILE_DRIVER" to true.

--- src/main.php
+++ src/main.php
@@ -2,15 +2,14 @@
 
 namespace {
 
-    use Amp\File\File;
-
     use function Amp\File\openFile;
+
+    use CatPaw\Amp\File\CatPawFile;
     
-
+    // benchmarks
     function main() {
-        /** @var File */
-        $file = yield openFile("output.txt", "w+");
-
+        /** @var CatPawFile */
+        $file  = yield openFile("output.txt", "w+");
         $chunk = str_repeat('a', 1_000);
         for ($i = 0; $i < 1_000_000; $i++) {
             yield $file->write($chunk);
