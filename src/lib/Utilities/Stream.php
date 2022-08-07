cf2e58912b8d8da4440839b8de483a86ee6bcc2e
tncrazvan <tangent.jotey@gmail.com>
2022-08-07 21:12:47 +0200

    Stream chunk size fixed

--- src/lib/Utilities/Stream.php
+++ src/lib/Utilities/Stream.php
@@ -9,23 +9,29 @@ use Amp\Promise;
 class Stream {
     private ?ResourceOutputStream $writer = null;
     private ?ResourceInputStream $reader  = null;
-    private function __construct(private mixed $stream) {
+    private function __construct(
+        private mixed $resource,
+        private ?int $chunkSize = null,
+    ) {
     }
 
-    public static function of(mixed $resource):self {
-        return new self($resource);
+    public static function of(
+        mixed $resource,
+        ?int $chunkSize = null
+    ):self {
+        return new self($resource, $chunkSize);
     }
 
     public function write(string $chunk):Promise {
         if (!$this->writer) {
-            $this->writer = new ResourceOutputStream($this->stream);
+            $this->writer = new ResourceOutputStream($this->resource, $this->chunkSize);
         }
         return $this->writer->write($chunk);
     }
 
     public function read():Promise {
         if (!$this->reader) {
-            $this->reader = new ResourceInputStream($this->stream);
+            $this->reader = new ResourceInputStream($this->resource, $this->chunkSize ?? ResourceInputStream::DEFAULT_CHUNK_SIZE);
         }
         return $this->reader->read();
     }
