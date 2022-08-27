a095783f089d942b0a106313ae4d986780f01f9b
tncrazvan <tangent.jotey@gmail.com>
2022-08-27 22:05:25 +0200

    fixed an issue with the EnvironmentService which caused the yaml environment file parser to crash if the yaml file was empty

--- src/lib/Environment/Services/EnvironmentService.php
+++ src/lib/Environment/Services/EnvironmentService.php
@@ -59,6 +59,8 @@ class EnvironmentService {
         EnvironmentConfigurationService $environmentConfigurationService
     ):Promise {
         return call(function() use ($logger, $environmentConfigurationService) {
+            $this->variables = [];
+
             $stringifiedFileNames = join(',', $environmentConfigurationService->getFileNames());
             
             if (!$fileName = yield $this->findFileName($environmentConfigurationService)) {
@@ -75,15 +77,17 @@ class EnvironmentService {
                 $contents .= $chunk;
             }
             
-            if (\str_ends_with($fileName, '.yml') || \str_ends_with($fileName, '.yaml')) {
-                if (\function_exists('yaml_parse')) {
-                    $this->variables = \yaml_parse($contents);
+            if (trim($contents) !== '') {
+                if (\str_ends_with($fileName, '.yml') || \str_ends_with($fileName, '.yaml')) {
+                    if (\function_exists('yaml_parse')) {
+                        $this->variables = \yaml_parse($contents);
+                    } else {
+                        $logger->error("Could not parse environment file, the yaml extension is needed in order to parse yaml environment files.");
+                        $this->variables = [];
+                    }
                 } else {
-                    $logger->error("Could not parse environment file, the yaml extension is needed in order to parse yaml environment files.");
-                    $this->variables = [];
+                    $this->variables = \Dotenv\Dotenv::parse($contents);
                 }
-            } else {
-                $this->variables = \Dotenv\Dotenv::parse($contents);
             }
 
             $_ENV = [
