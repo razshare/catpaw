1ffc24e65016663f03d5b9c4a0cf09b5eb8ed6c6
tncrazvan <tangent.jotey@gmail.com>
2022-08-27 19:31:09 +0200

    Version 0.2.74

---
+++ src/lib/Attributes/Arguments.php
@@ -0,0 +1,36 @@
+<?php
+namespace CatPaw\Attributes;
+
+use Attribute;
+use CatPaw\Attributes\Interfaces\AttributeInterface;
+
+use CatPaw\Attributes\Traits\CoreAttributeDefinition;
+use ReflectionParameter;
+
+#[Attribute]
+class Arguments implements AttributeInterface {
+    use CoreAttributeDefinition;
+
+    private static bool $initialized = false;
+    private static array $cache      = [];
+
+    public function __construct() {
+        global $argv;
+
+        if (!self::$initialized) {
+            self::$initialized = true;
+            foreach ($argv as $i => $value) {
+                if (0 === $i || str_starts_with($value, '-')) {
+                    continue;
+                }
+                self::$cache[] = $value;
+            }
+        }
+    }
+
+    public function onParameter(ReflectionParameter $reflection, mixed &$value, mixed $context) {
+        /** @var array<string|int|bool|float> $value */
+        /** @var false $context */
+        $value = self::$cache;
+    }
+}
---
+++ src/lib/Attributes/Option.php
@@ -0,0 +1,49 @@
+<?php
+namespace CatPaw\Attributes;
+
+use Attribute;
+use CatPaw\Attributes\Interfaces\AttributeInterface;
+
+use CatPaw\Attributes\Traits\CoreAttributeDefinition;
+use ReflectionParameter;
+
+#[Attribute]
+class Option implements AttributeInterface {
+    use CoreAttributeDefinition;
+
+    private static bool $initialized = false;
+    private static array $cache      = [];
+    private array $options           = [];
+
+    public function __construct(private string $name) {
+        global $argv;
+
+        if (!self::$initialized) {
+            self::$initialized = true;
+            foreach ($argv as $i => $value) {
+                if (0 === $i || !str_starts_with($value, '-')) {
+                    continue;
+                }
+                $this->options[] = $value;
+            }
+        }
+    }
+
+    private function findOptionByName(string $name):string {
+        if (isset(self::$cache[$name])) {
+            return self::$cache[$name];
+        }
+        foreach ($this->options as $i => $value) {
+            if (str_starts_with($value, $name)) {
+                return self::$cache[$name] = trim(substr($value, strlen($name)));
+            }
+        }
+        return self::$cache[$name] = '';
+    }
+
+    public function onParameter(ReflectionParameter $reflection, mixed &$value, mixed $context) {
+        /** @var string|int|bool|float $value */
+        /** @var false $context */
+        $value = $this->findOptionByName($this->name);
+    }
+}
