4a641412d2bf92fa9150df77a981406f1385d908
tncrazvan <tangent.jotey@gmail.com>
2022-09-30 18:28:52 +0200

    Version 0.4.24

--- src/lib/Environment/Services/EnvironmentService.php
+++ src/lib/Environment/Services/EnvironmentService.php
@@ -115,7 +115,8 @@ class EnvironmentService {
             if (trim($contents) !== '') {
                 if (\str_ends_with($fileName, '.yml') || \str_ends_with($fileName, '.yaml')) {
                     if (\function_exists('yaml_parse')) {
-                        $this->variables = \yaml_parse($contents);
+                        $vars            = \yaml_parse($contents);
+                        $this->variables = $vars?$vars:[];
                     } else {
                         $this->logger->error("Could not parse environment file, the yaml extension is needed in order to parse yaml environment files.");
                         $this->variables = [];
