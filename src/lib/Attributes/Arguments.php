f85de93eb97a88b7fb020c4ffcd029057ed8d758
razvan <razvan@pop-os.localdomain>
2023-04-27 22:22:32 +0200

    fixing readline

--- src/lib/Attributes/Arguments.php
+++ src/lib/Attributes/Arguments.php
@@ -5,6 +5,7 @@ use Attribute;
 use CatPaw\Attributes\Interfaces\AttributeInterface;
 
 use CatPaw\Attributes\Traits\CoreAttributeDefinition;
+use CatPaw\Utilities\ReflectionTypeManager;
 use ReflectionParameter;
 
 #[Attribute]
@@ -13,18 +14,31 @@ class Arguments implements AttributeInterface {
 
     private static bool $initialized = false;
     private static array $cache      = [];
+    private static array $blueprints = [];
 
-    public function __construct() {
+    private const PATTERN_PARAM = '/^\{[0-9]*\w+\}$/';
+
+    public function __construct(private string $template) {
         self::init();
+
+        if ($pieces = preg_split('/\s+/i', $this->template)) {
+            foreach ($pieces as $key => $value) {
+                if (preg_match(self::PATTERN_PARAM, $value)) {
+                    self::$blueprints[$template][] = self::$cache[$key] ?? false;
+                } else if (str_starts_with($value, '{') && str_ends_with($value, '}')) {
+                    echo "Invalid placeholder `$value` for template `$template`".PHP_EOL;
+                    die(22);
+                }
+            }
+        }
     }
 
     public static function init() {
         global $argv;
-
         if (!self::$initialized) {
             self::$initialized = true;
             foreach ($argv as $i => $value) {
-                if (0 === $i || Option::exists($value)) {
+                if (0 === $i || str_starts_with($value, '-')) {
                     continue;
                 }
                 self::$cache[] = $value;
@@ -35,6 +49,11 @@ class Arguments implements AttributeInterface {
     public function onParameterMount(ReflectionParameter $reflection, mixed &$value, mixed $context) {
         /** @var array<string|int|bool|float> $value */
         /** @var false $context */
-        $value = self::$cache;
+
+        if (ReflectionTypeManager::unwrap($reflection)->getName() === 'array') {
+            $value = self::$blueprints[$this->template];
+        } else {
+            $value = self::$cache;
+        }
     }
 }
