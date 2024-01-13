c37d10eb9acf8d8c4841def8bb20942023d73374
Razvan <tangent.jotey@gmail.com>
2024-01-13 16:49:56 +0100

    feat(build): adding a tips option (more tips to come)

--- src/main.php
+++ src/main.php
@@ -1,28 +1,69 @@
 <?php
 
-use CatPaw\Core\Attributes\Option;
-use CatPaw\Core\Unsafe;
 use function CatPaw\Core\anyError;
+use CatPaw\Core\Attributes\Option;
 use function CatPaw\Core\Build\build;
+
+use function CatPaw\Core\error;
+use CatPaw\Core\File;
+use function CatPaw\Core\ok;
+use function CatPaw\Core\out;
+use CatPaw\Core\Unsafe;
+use function CatPaw\Text\foreground;
+
+use function CatPaw\Text\nocolor;
 
 /**
  * @return Unsafe<void>
  */
 function main(
+    // ===> BUILD
     #[Option("--build")]
-    bool         $build = false,
+    bool $build = false,
     #[Option("--build-config-init")]
-    bool         $buildConfigInit = false,
+    bool $buildConfigInit = false,
     #[Option("--build-config")]
     false|string $buildConfig = false,
     #[Option("--build-optimize")]
-    bool         $buildOptimize = false,
+    bool $buildOptimize = false,
+
+    // ===> TIPS
+    #[Option("--tips")]
+    bool $tips,
 ): Unsafe {
-    return anyError(fn() => match (true) {
+    return anyError(fn () => match (true) {
         $build => build(
             buildConfig    : $buildConfig,
             buildConfigInit: $buildConfigInit,
             buildOptimize  : $buildOptimize,
-        )
+        ),
+        $tips => tips(),
     });
+}
+
+function tips() {
+    try {
+        $message = '';
+
+        if (
+            !File::exists('.pre-commit-config.yaml')
+            && !File::exists('.pre-commit-config.yml')
+        ) {
+            $message = join([
+                foreground(170, 140, 40),
+                "Remember to run `",
+                foreground(140, 170, 40),
+                "composer dev:precommit",
+                foreground(170, 140, 40),
+                "` if you want to sanitize your code before committing.",
+                nocolor(),
+            ]);
+        }
+
+
+        out()->write($message);
+        return ok();
+    } catch (\Throwable $error) {
+        return error($error);
+    }
 }
