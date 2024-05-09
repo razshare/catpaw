e3e746c1e6286b2975d7a1d4a5c9bfbe48b45bb9
Razvan Tanase <tangent.jotey@gmail.com>
2024-05-09 19:38:37 +0200

    feat(OpenApiService): fixing method name

--- src/lib/Web/Services/OpenApiService.php
+++ src/lib/Web/Services/OpenApiService.php
@@ -16,7 +16,7 @@ class OpenApiService {
      * You can safely expose this through a rest api.
      * @return array<mixed>
      */
-    public function data():array {
+    public function getData():array {
         return $this->openApiStateService->json;
     }
 
