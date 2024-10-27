5d24ea841b7fb0274952066234c088ab6d68c350
Razvan <razvan@razshare.dev>
2024-10-27 20:57:50 +0100

    chore(app): dropping go compilation features

--- src/main.php
+++ src/main.php
@@ -7,7 +7,6 @@ use CatPaw\Core\CommandBuilder;
 use CatPaw\Core\CommandContext;
 
 use function CatPaw\Core\error;
-use function CatPaw\Core\execute;
 
 use CatPaw\Core\File;
 use CatPaw\Core\Interfaces\CommandInterface;
@@ -111,40 +110,6 @@ class HiCommand implements CommandRunnerInterface {
     }
 }
 
-class GompileCommand implements CommandRunnerInterface {
-    public function __construct(private LoggerInterface $logger) {
-    }
-
-    public function build(CommandBuilder $builder): Result {
-        $builder->withOption('g', 'gompile', error("A Go file name is required."));
-        return ok();
-    }
-
-    public function run(CommandContext $context): Result {
-        return anyError(function() use ($context) {
-            $inputFileName = $context->get('gompile')->try();
-            if (!$fullFileName = realpath($inputFileName)) {
-                return error("File `{$inputFileName}` not found.");
-            }
-            
-            if (!preg_match('/(.+).go$/', $fullFileName, $matches)) {
-                return error("Illegal Go file name received `$fullFileName`, please provide a file name that ends with `.go`.");
-            }
-
-            $fileName      = basename($matches[1]);
-            $directoryName = dirname($matches[1]);
-
-            execute("GOOS=linux CGO_ENABLED=1 go build -o $fileName.so -buildmode=c-shared $fileName.go", out(), $directoryName)->try();
-            execute("cpp -P $fileName.h $fileName.static.h", out(), $directoryName)->try();
-            File::delete("$directoryName/$fileName.h")->try();
-
-            $this->logger->info("Go program compiled successfully into `$directoryName/$fileName.so`.");
-
-            return ok();
-        });
-    }
-}
-
 class HelpCommand implements CommandRunnerInterface {
     public function build(CommandBuilder $builder):Result {
         return ok();
@@ -154,7 +119,6 @@ class HelpCommand implements CommandRunnerInterface {
         echo <<<BASH
             \n
             -b,  --build [-o, --optimize]        Builds the project into a .phar.
-            -g,  --gompile                       Compile a Go program into a shared object and a static header file as required by Php's FFI api.
             -t,  --tips                          Some tips.
             -h,  --hi                            Says hi. Useful for debugging.
             -i,  --install-pre-commit            Installs the pre-commit hook.
@@ -172,7 +136,6 @@ class HelpCommand implements CommandRunnerInterface {
 function main(CommandInterface $command, LoggerInterface $logger) {
     return anyError(fn () => match (true) {
         $command->register(new BuildCommand)->try()              => ok(),
-        $command->register(new GompileCommand($logger))->try()   => ok(),
         $command->register(new TipsCommand)->try()               => ok(),
         $command->register(new HiCommand)->try()                 => ok(),
         $command->register(new InstallPreCommitCommand)->try()   => ok(),
