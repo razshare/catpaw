1ef13a6d6055295adf13e432b2bb8ca2ee28eb7e
Razvan <razvan@razshare.dev>
2024-11-16 07:10:59 +0100

    chore(preview): fixing test endpoint

--- preview/api/get.php
+++ preview/api/get.php
@@ -1,8 +1,2 @@
 <?php
-// src/api/get.php
-use CatPaw\Web\Attributes\ProducesItem;
-use function CatPaw\Web\success;
-
-return
-    #[ProducesItem(200, 'text/plain', 'Success!', string::class)]
-    fn () => success('hello')->item();
+return fn () => "test";
